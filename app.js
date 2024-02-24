0d0b80b7c9853fde7d8e7adfeef94f08ab243b90
yanil8068 <yanil8068@gmail.com>
2024-02-24 12:14:29 +0530

    Listing Model

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const Listing = require("./models/listing.js")
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -20,6 +20,23 @@ async function main(){
 app.get("/", (req, res)=>{
     res.send("Hi, i am root");
 })
+
+app.get("/testListing", async (req, res)=>{
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1200,
+        location: "Calangute , Goa",
+        country: "India",
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successful testing");
+});
+
+
+
+
 
 app.listen(8080, ()=>{
     console.log("server is listening to port 8080");
