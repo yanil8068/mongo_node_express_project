63ade66c8022a77728e280f6d86c2293e7d14ac3
yanil8068 <yanil8068@gmail.com>
2024-02-29 10:38:28 +0530

    Creating Boilerplate

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const Listing = require("./models/listing.js")
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.engine("ejs", ejsMate);
 
 main()
 .then(()=>{
