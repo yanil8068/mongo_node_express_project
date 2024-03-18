745bdba01be1cbb709414cb6dafed6007b4a9101
yanil8068 <yanil8068@gmail.com>
2024-03-18 21:33:09 +0530

    Add ExpressError

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.engine("ejs", ejsMate);
 
@@ -29,14 +31,14 @@ async function main(){
 }
 
 
-app.get("/", (req, res)=>{
+app.get("/", wrapAsync((req, res)=>{
     res.send("Hi, i am root");
-})
+}))
 
-app.get("/listings", async (req, res)=>{
+app.get("/listings", wrapAsync(async (req, res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-})
+}))
 
 //New Route
 app.get("/listings/new", (req, res)=>{
@@ -45,8 +47,11 @@ app.get("/listings/new", (req, res)=>{
 
 //Create Route
 app.post("/listings", wrapAsync(async(req, res, next)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400, "Send valid data for listing")
+    }
    
-        let {title,description,image,price,country,location}=req.body;
+    let {title,description,image,price,country,location}=req.body;
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -54,33 +59,36 @@ app.post("/listings", wrapAsync(async(req, res, next)=>{
 }));
 
 // show route
-app.get("/listings/:id", async(req, res)=>{
+app.get("/listings/:id", wrapAsync(async(req, res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
-});
+}));
 
 //Edit Route
-app.get("/listings/:id/edit", async(req, res)=>{
+app.get("/listings/:id/edit", wrapAsync(async(req, res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", {listing});
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res)=> {
+app.put("/listings/:id", wrapAsync(async (req, res)=> {
+    if(!req.body.listing){
+        throw new ExpressError(400, "Send valid data for listing");
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {... req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete route
-app.delete("/listings/:id", async (req, res)=>{
+app.delete("/listings/:id", wrapAsync(async (req, res)=>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing", async (req, res)=>{
 //     let sampleListing = new Listing({
@@ -95,11 +103,19 @@ app.delete("/listings/:id", async (req, res)=>{
 //     res.send("successful testing");
 // });
 
-
+//standard response render if request does not match to any of the above routes
+app.all("*",(req, res, next)=>{
+    next(new ExpressError(404, "Page Not Found!"));
+});
 
 app.use((err, req, res, next)=>{
-    res.send("something went wrong!");
-})
+   let {statusCode = 500, message = "something went wrong!"} = err;
+   res.status(statusCode).send(message);
+});
+
+
+
+
 
 app.listen(8080, ()=>{
     console.log("server is listening to port 8080");
