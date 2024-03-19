f55698475d9a8da8a0a99025ab2399b123c6c021
yanil8068 <yanil8068@gmail.com>
2024-03-19 19:56:31 +0530

    Error.ejs

--- app.js
+++ app.js
@@ -110,7 +110,8 @@ app.all("*",(req, res, next)=>{
 
 app.use((err, req, res, next)=>{
    let {statusCode = 500, message = "something went wrong!"} = err;
-   res.status(statusCode).send(message);
+   res.render("error.ejs", {message});
+//    res.status(statusCode).send(message);
 });
 
 
