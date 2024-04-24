401009dcf3c5b9e28a20967b4424adb4638317b2
yanil8068 <yanil8068@gmail.com>
2024-04-24 21:38:27 +0530

    Cookie in Session Options

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ const sessionOptions = {
   secret: "mysupersecretcode",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 app.use(session(sessionOptions));
 
