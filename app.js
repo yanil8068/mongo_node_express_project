0e760cdda9917949e26aa99e18c4af593798868d
yanil8068 <yanil8068@gmail.com>
2024-04-25 15:27:10 +0530

    SignUp User-Get

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
-const reviews = require("./routes/review.js");
-const listings = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const userRouter = require("./routes/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
@@ -59,8 +60,9 @@ app.get("/demouser", async (req, res) => {
   res.send(registerUser);
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.engine("ejs", ejsMate);
 
