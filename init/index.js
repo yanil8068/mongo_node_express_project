3bc622c44b04ae8dc72cbfa7c18b7c3a0ea57f13
yanil8068 <yanil8068@gmail.com>
2024-04-26 15:16:09 +0530

    Listing Owner

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "662a24e1f4ae8e6d14202098",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
--- routes/listing.js
+++ routes/listing.js
@@ -38,11 +38,14 @@ router.get(
   "/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id)
+      .populate("reviews")
+      .populate("owner");
     if (!listing) {
       req.flash("error", "Listing you requested for does not exist!");
       res.redirect("/listings");
     }
+    console.log(listing);
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -55,6 +58,7 @@ router.post(
   wrapAsync(async (req, res, next) => {
     // let { title, description, image, price, country, location } = req.body;
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
