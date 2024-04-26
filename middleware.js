a63d0adfead6193003c961141555614b28afebe7
yanil8068 <yanil8068@gmail.com>
2024-04-26 13:43:36 +0530

    pos-login page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
+  //console.log(req.path, "...", req.originalUrl);
   if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
     req.flash("error", "you must be logged in to create listing!");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
