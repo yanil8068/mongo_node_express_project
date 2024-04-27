0c7816f9ea0995c65b3629112944f830391412fa
yanil8068 <yanil8068@gmail.com>
2024-04-27 14:01:57 +0530

    Store Files(in cloudinary)

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,8 @@ const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 const listingController = require("../controllers/listings.js");
 
