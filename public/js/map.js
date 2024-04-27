d4e517534bcff4320f7b8a30939f1ac58d40c0aa
yanil8068 <yanil8068@gmail.com>
2024-04-27 16:50:43 +0530

    Map Popup

--- public/js/map.js
+++ public/js/map.js
@@ -2,9 +2,20 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: coordinates, // starting position [lng, lat]
+  center: listing.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 const marker = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(coordinates)
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
+    )
+  )
   .addTo(map);
+
+// const popup = new mapboxgl.Popup()
+//   .setLngLat(listing.geometry.coordinates)
+//   .setHTML("<h1>Hello World!</h1>")
+//   .setMaxWidth("300px")
+//   .addTo(map);
