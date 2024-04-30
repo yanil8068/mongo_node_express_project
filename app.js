186f2accfe35b62fbd17406b3f80adaeaad38703
yanil8068 <yanil8068@gmail.com>
2024-04-30 10:21:46 +0530

    Push code to github

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 const dbUrl = process.env.ATLASDB_URL;
 
 const path = require("path");
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: "mysupersecretcode",
+    secret: "process.env.SECRET",
   },
   touchAfter: 24 * 3600,
 });
@@ -48,7 +48,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: "process.env.SECRET",
   resave: false,
   saveUninitialized: true,
   cookie: {
